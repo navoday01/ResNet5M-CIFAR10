1d786a1e2f9f25f7d9dc158feaaf2bfecd5bcbdd
jagennath-hari <110366406+jagennath-hari@users.noreply.github.com>
2022-11-22 00:52:03 -0500

    Update ResNetModel.py

--- models/ResNetModel.py
+++ models/ResNetModel.py
@@ -66,11 +66,11 @@ class ResNet(nn.Module):
 
         self.conv1 = nn.Conv2d(3, channels[0], kernel_size = 3, stride = 1, padding = 1, bias = False)
         self.bn1 = nn.BatchNorm2d(channels[0])
-        self.layer1 = self._make_layer(block, channels[0], num_blocks[0], stride = 1)
-        self.layer2 = self._make_layer(block, channels[1], num_blocks[1], stride = 2)
-        self.layer3 = self._make_layer(block, channels[2], num_blocks[2], stride = 2)
-        self.layer4 = self._make_layer(block, channels[3], num_blocks[3], stride = 2)
-        self.linear = nn.Linear(channels[3] * block.expansion, num_classes)
+        self.layer1 = self._make_layer(block[0], channels[0], num_blocks[0], stride = 1)
+        self.layer2 = self._make_layer(block[1], channels[1], num_blocks[1], stride = 2)
+        self.layer3 = self._make_layer(block[2], channels[2], num_blocks[2], stride = 2)
+        self.layer4 = self._make_layer(block[3], channels[3], num_blocks[3], stride = 2)
+        self.linear = nn.Linear(channels[3] * block[3].expansion, num_classes)
 
     def _make_layer(self, block, out_channels, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
